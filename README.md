d1cacb6063b3ebe85a7c4c8efc923b382e486fc8
Raimundo Andrade de Lima Neto <andradeneto1999@gmail.com>
2024-10-17 20:44:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sorteio_carta
+ sorteio carta
---
+++ script.js
@@ -0,0 +1,34 @@
+let usedNumbers = {'BIOLÓGICO': [], 'FÍSICO': [], 'QUÍMICO': [], 'ERGONÔMICO': [], 'ACIDENTE': []};
+// const maxNumbers = 5;
+const has_tab = {'BIOLÓGICO': 61, 'FÍSICO': 19, 'QUÍMICO': 20, 'ERGONÔMICO': 12, 'ACIDENTE': 28};
+const selectOption = document.getElementById('mySelect');
+
+
+function generateRandomNumber() {
+    // If all numbers have been used, reset the list
+    let SelectedValue = selectOption.value;
+    maxNumbers = has_tab[SelectedValue];
+    if (usedNumbers[SelectedValue].length === maxNumbers) {
+        usedNumbers[SelectedValue] = [];
+    }
+
+    // Generate a random number that hasn't been used yet
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(Math.random() * maxNumbers) + 1;
+    } while (usedNumbers[SelectedValue].includes(randomNumber));
+
+    // Add the new number to the usedNumbers list
+    usedNumbers[SelectedValue].push(randomNumber);
+    print(usedNumbers[SelectedValue]);
+    // Display the generated number
+    // document.getElementById('random-number').src = call_image(randomNumber).then(function (res) {
+    //     return res.text();
+    // });
+    document.getElementById('random-number').src = call_image(SelectedValue,randomNumber);
+    // document.getElementById('random-number').innerText = randomNumber;
+}
+
+function call_image(SelectedValue,num){
+    return `${SelectedValue}/${num}.jpg`;
+}
